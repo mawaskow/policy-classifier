3bf981132f5bf57b0a791df5728ce4c0f21a1a13
Al <mawaskow@yahoo.com>
2024-08-07 00:41:44 +0100

    gets information but doesn't store item/download

--- policy_scraping/policy_scraping/spiders/eu_necp_spider.py
+++ policy_scraping/policy_scraping/spiders/eu_necp_spider.py
@@ -24,52 +24,41 @@ class NECPSpider(BaseSpider):
 
     def start_requests(self):
         url = "https://commission.europa.eu/energy-climate-change-environment/implementation-eu-countries/energy-and-climate-governance-and-reporting/national-energy-and-climate-plans_en#national-energy-and-climate-plans-2021-2030"
-        yield scrapy.Request(url, self.parse_dept)
+        yield scrapy.Request(url, self.get_natns)
 
-    def parse_dept(self, response):
-        # gets links to departments from policies landing page
-        results_sel = response.selector.xpath('//div[@class="ecl"]')
-        # points links to the publications tab of each department
-        dept_hrefs = [link.attrib["href"]+"latest/" for link in results_sel.xpath('.//a')]
-        search_lnks = []
-        # points links to search results filtered by type and keyword
-        for dept in dept_hrefs:
-            for key in sr_kw_dct:
-                search_lnks.append(dept+f"?q={key}&sort_by=published_date&type=policy_information&type=general_publications")
-        yield from response.follow_all(search_lnks, self.nav_dept_pg)
-    
-    def nav_dept_pg(self, response):
-        # get links to all results on current search page
-        results_sel = response.selector.xpath('//div[contains(@class, "reboot-content")]//ul')
-        pol_links = [link.attrib["href"] for link in results_sel.xpath('.//a')]
-        yield from response.follow_all(pol_links, self.parse)
-        # if there is another page, go to the next page so the above can be executed again
-        pg_count = response.selector.xpath('//div[@title="Pagination"]//div[contains(@class,"text-center")]/text()').get()
-        if int(pg_count.split("/")[0]) < int(pg_count.split("/")[1]):
-            page_next = response.selector.xpath('//div[@title="Pagination"]//a').attrib["href"]
-            yield response.follow(page_next, self.nav_dept_pg)
-
-    def parse(self, response):
-        # on the page of the result entry
-        results_sel = response.selector.xpath('//div[@id="main"]/div/div/div/div')
-        pg_title = results_sel.xpath(".//h1/text()").get()
-        # if the search antikeywords arent present
-        if not any(akwd in pg_title.lower() for akwd in sr_akw_dct):
-            # get all possible download docs on page
-            results_pdfs = response.selector.xpath('//a[text()="Download"]')
-            for result_pdf in results_pdfs:
-                doc_title = result_pdf.xpath('../../div/p[contains(@id,"download_title")]/text()').get()
-                if not any(akwd in doc_title.lower() for akwd in doc_akw_dct):
-                    doc_itm = IrishGovPolicy()
-                    doc_itm["pg_title"] = pg_title
-                    doc_itm["pg_link"] = response.request.url
-                    doc_itm["publication_date"] = results_sel.xpath(".//p[text()[contains(.,'Published')]]/time").attrib["datetime"]
-                    doc_itm["department"] = results_sel.xpath(".//p[text()[contains(.,'From')]]/a/text()").get()
-                    doc_itm["type"] = results_sel.xpath(".//span/text()").get().strip()
-                    #
-                    doc_itm["doc_title"] = doc_title
-                    path = result_pdf.xpath(".").attrib["href"]
-                    doc_itm["file_urls"] = [path]
-                    hash = hashlib.sha1(path.encode()).hexdigest()
-                    doc_itm["hash_name"] = hash
-                    yield doc_itm
+    def get_natns(self, response):
+        # gets list of all countries NECPs
+        #countries = response.selector.xpath('//div[@class="ecl-accordion"]//span[@class="ecl-accordion__toggle-title"]/text()').getall()
+        dct = {}
+        #for i in countries:
+        #    dct[i] = {}
+        
+        # select all NECP country items
+        results_sel = response.selector.xpath('//div[@class="ecl-accordion"]//div[@class="ecl-accordion__item"]')
+        for resp in results_sel:
+            cntry = resp.xpath('.//span[@class="ecl-accordion__toggle-title"]/text()').get()
+            dct[cntry] = {}
+            getli = resp.xpath('.//div[@class="ecl"]/ul/li[not(contains(text(),"Draft") or contains(text(),"Commission"))]')
+            for li in getli:
+                litxt = li.xpath('./text()').get()
+                if litxt:
+                    if "Final updated NECP" in litxt:
+                        dct[cntry]["Final updated NECP"] = {}
+                        ael = li.xpath('./a')
+                        for lnk in ael:
+                            name = lnk.xpath('./text()').get()
+                            dct[cntry]["Final updated NECP"][name]= lnk.attrib['href']
+                    else:
+                        aes = li.xpath('./a[not(contains(text(),"Draft") or contains(text(),"Commission"))]')
+                        for lnk in aes:
+                            lnktxt = lnk.xpath('./text()').get()
+                            if "Commission" in lnktxt:
+                                pass
+                            elif "factsheet" in lnktxt:
+                                pass
+                            elif "Staff Working Document" in lnktxt:
+                                pass
+                            else:
+                                dct[cntry][lnktxt]= lnk.attrib['href']
+        print(dct)
+        yield dct
